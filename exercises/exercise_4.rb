19ddc09e61f28fa5413cb12023d6eb214691b616
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-10-13 08:41:24 +0000

    update delete and view stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,24 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+stores = Store.create([{
+  name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true
+},
+{
+  name: "Whistler", annual_revenue: 1900000, mens_apparel: false, womens_apparel: false
+}, 
+{
+  name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true
+}])
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "#{store.name} and #{store.annual_revenue}"
+end
+puts "-------------------------------------------------"
+
+@womens_apparel = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+@womens_apparel.each do |store|
+  puts "#{store.name}"
+end
